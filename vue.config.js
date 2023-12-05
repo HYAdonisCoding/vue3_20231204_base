20668e5ac73f637e6698dfc83b0d172dc5597aa7
Eason <296786475@qq.com>
2023-12-05 10:55:53 +0800

    reactive函数的使用、vue3的中响应式原理

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
 const {
   defineConfig
 } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // 关闭语法检查
+
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap((args) => {
+      args[0].title = 'Vue3_Base';// 更改根目录页签标题
+      return args
+    })
+  },
 })
